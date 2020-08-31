54f94522cc02675e9e51724f3c52165512d99d8d
MV <marcos.vinicius124816@gmail.com>
2020-08-31 09:33:19 -0300

    1

---
+++ udpclient.py
@@ -0,0 +1,11 @@
+
+from socket import *
+serverName = '192.168.16.105'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+message = raw_input('Input lowercase sentence:')
+clientSocket.sendto(message,(serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print modifiedMessage
+clientSocket.close()
+
